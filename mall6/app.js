687d794b8b7b54baab43b6af48ce7caa9d04292c
max-translia <32696645+max-translia@users.noreply.github.com>
2019-09-26 13:45:54 +0800

    添加跨域的配置

--- mall6/app.js
+++ mall6/app.js
@@ -19,6 +19,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use('*', function(req, res, next) {
+
+  res.header('Access-Control-Allow-Origin', req.headers.origin);//注意这里不能使用 *
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header("X-Powered-By",' 3.2.1');
+  res.header('Access-Control-Allow-Credentials', true); // 允许服务器端发送Cookie数据
+  res.header("Content-Type", "application/json;charset=utf-8");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');//设置方法
+  if (req.method == 'OPTIONS') {
+    res.sendStatus(200); // 在正常的请求之前，会发送一个验证，是否可以请求。
+  }
+  else {
+
+    next();
+  }
+
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
