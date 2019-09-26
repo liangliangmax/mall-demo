c750aea6b09aaa90e84a73df5b1cc8d977aaba9d
max-translia <32696645+max-translia@users.noreply.github.com>
2019-09-26 15:21:00 +0800

    优化结构

--- mall7/config/mongoConfig.js
+++ mall7/config/mongoConfig.js
@@ -1,9 +1,9 @@
 let mongoConfig = {};
 
-mongoConfig.host = process.env.mongo.host ||'10.4.249.94';
-mongoConfig.user = process.env.mongo.user ||'root';
-mongoConfig.password = process.env.mongo.password ||'root';
-mongoConfig.port = process.env.mongo.port ||'27017';
-mongoConfig.database = process.env.mongo.database ||'liang-test';
+mongoConfig.host = process.env.mongoHost ||'10.4.249.94';
+mongoConfig.user = process.env.mongouUser ||'root';
+mongoConfig.password = process.env.mongoPassword ||'root';
+mongoConfig.port = process.env.mongoPort ||'27017';
+mongoConfig.database = process.env.mongoDatabase ||'liang-test';
 
 module.exports = mongoConfig;
--- mall7/config/mysqlConfig.js
+++ mall7/config/mysqlConfig.js
@@ -1,10 +1,10 @@
 let dbConfig = {};
 
 
-dbConfig.host = process.env.mongo.host ||'10.4.249.94';
-dbConfig.user = process.env.mongo.user ||'root';
-dbConfig.password = process.env.mongo.password ||'root';
-dbConfig.port = process.env.mongo.port ||'3306';
-dbConfig.database = process.env.mongo.database ||'liang';
+dbConfig.host = process.env.dbHost ||'10.4.249.94';
+dbConfig.user = process.env.dbUser ||'root';
+dbConfig.password = process.env.dbPassword ||'root';
+dbConfig.port = process.env.dbPort ||'3306';
+dbConfig.database = process.env.dbDatabase ||'liang';
 
 module.exports = dbConfig;
